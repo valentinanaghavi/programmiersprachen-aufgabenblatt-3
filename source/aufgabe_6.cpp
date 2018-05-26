ddfcbe4464c4278754d08f0a9fb6f616efab7953
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-05-26 13:18:11 +0200

    add  3.7

--- source/aufgabe_6.cpp
+++ source/aufgabe_6.cpp
@@ -21,10 +21,18 @@ int main(int argc, char * argv[]) {
 
 TEST_CASE("circle_sort", "[sort]") //Aufgabe 3.6
 {
-	std::vector<Circle> sorted_circles{ 1,3,2,5,4,6}; 
+	std::vector<Circle> sorted_circles{ 1,9,2,5,4,3}; 
 
 	std::sort(sorted_circles.begin(),sorted_circles.end());
 
-
 	REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
+}
+
+TEST_CASE("circle_sort_lambda", "[sort]") //Aufgabe 3.7
+{
+	std::vector<Circle> sorted_circles{ 1,9,2,5,4,3}; 
+
+	std::sort(sorted_circles.begin(),  sorted_circles.end(), [] (Circle const& c1,Circle const& c2) { return c1 < c2; });
+
+	REQUIRE (std::is_sorted( sorted_circles.begin(),  sorted_circles.end ()));
 }
